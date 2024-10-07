546279d39c0a6d56261c7dcd926d7c1b6e7ff8ac
Nate Sawant <natesawant@gmail.com>
2024-10-07 02:00:43 -0400

    Add images

--- app.py
+++ app.py
@@ -9,17 +9,29 @@ df = pd.read_csv(
 app = Dash()
 
 app.layout = [
-    html.H1(children="Title of Dash App", style={"textAlign": "center"}),
-    dcc.Dropdown(df.country.unique(), "Canada", id="dropdown-selection"),
-    dcc.Graph(id="graph-content"),
+    html.H1(children="F1 Visualizations", style={"textAlign": "center"}),
+    html.Div(
+        [
+            html.Div(
+                [
+                    html.Img(src=app.get_asset_url("old-f1-car.png")),
+                    html.Img(src=app.get_asset_url("new-f1-car.png")),
+                ],
+                style={"display": "flex", "justify-content": "space-between"},
+            ),
+            dcc.Slider(
+                1950,
+                2024,
+                1,
+                value=2024,
+                marks={1950: "1950", 1975: "1975", 2000: "2000", 2024: "2024"},
+                tooltip={"placement": "bottom", "always_visible": True},
+                id="year-selection",
+            ),
+        ],
+        style={"padding": "5%"},
+    ),
 ]
-
-
-@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
-def update_graph(value):
-    dff = df[df.country == value]
-    return px.line(dff, x="year", y="pop")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
